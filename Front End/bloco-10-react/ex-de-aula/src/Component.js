958421cca2b727b15826e347fee903920e94e3b4
Victor Vidal <victorvidal08@hotmail.com>
2022-04-04 19:07:56 -0300

    finaliza exemplos do dia

---
+++ Front End/bloco-10-react/ex-de-aula/src/Component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class MyName extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Meu nome é Victor!!</h1>
+            </div>
+        )
+    }
+}
+export default MyName;
