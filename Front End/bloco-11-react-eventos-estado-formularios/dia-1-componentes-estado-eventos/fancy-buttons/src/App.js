b28a9114abbe37ce47087ebb8b4afc438992b85a
Victor Vidal <victorvidal08@hotmail.com>
2022-04-07 18:22:12 -0300

    adiciona exemplo do dia finalizado

--- Front End/bloco-11-react-eventos-estado-formularios/dia-1-componentes-estado-eventos/fancy-buttons/src/App.js
+++ Front End/bloco-11-react-eventos-estado-formularios/dia-1-componentes-estado-eventos/fancy-buttons/src/App.js
@@ -1,32 +1,57 @@
 import React from 'react';
 import './App.css';
 
-/* Embora isso funcione, essa DEFINITIVAMENTE
-não é a maneira correta de se criar eventos
-em React! A função se refere ao componente,
-então deve ser parte de sua classe! */
-function handleClick() {
-  console.log('Clicou no botão!')
-}
-function handleClick1() {
-  console.log('Clicou no botão 1!')
-}
-function handleClick2() {
-  console.log('Clicou no botão 2!')
-}
-
-
 class App extends React.Component {
-  /* Repare que, diferentemente do HTML, no
-  JSX você associa uma função a um evento
-  passando a própria função entre chaves `{}` */
+  constructor() {
+    super()
+    this.state = {
+      numeroDeCliques: 0,
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+  }
+
+  handleClick =() => {
+    this.setState((estadoAnterior, _props) => ({
+    numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+  }))}
+  handleClick1 =() => {
+    this.setState((estadoAnterior1, _props) => ({
+    numeroDeCliques1: estadoAnterior1.numeroDeCliques1 + 1
+  }))}
+  handleClick2 =() => {
+    this.setState((estadoAnterior2, _props) => ({
+    numeroDeCliques2: estadoAnterior2.numeroDeCliques2 + 1
+  }))}
+
+  // apoio do gabarito
+  getButtonColor(num) {
+      // Essa função contém um ternário que realiza a lógica para mudarmos
+      // a cor do botão para verde quando for um número par
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+  
   render() {
+    const { numeroDeCliques, numeroDeCliques1, numeroDeCliques2 } = this.state;
     return <div>
-        <button onClick={handleClick}>Meu botão</button>
-        <button onClick={handleClick1}>Meu botão 1</button>
-        <button onClick={handleClick2}>Meu botão 2</button>
+        <h1>Os botões mudam de cor de acordo com a quantidade de clicks</h1>
+        <button 
+        onClick={this.handleClick}
+        style={{backgroundColor: this.getButtonColor(numeroDeCliques)}}
+        >Botão conta click {this.state.numeroDeCliques}</button>
+        <button 
+        onClick={this.handleClick1}
+        style={{backgroundColor:this.getButtonColor(numeroDeCliques1)}}
+        >{this.state.numeroDeCliques1}</button>
+        <button 
+        onClick={this.handleClick2}
+        style={{backgroundColor: this.getButtonColor(numeroDeCliques2)}}
+        >{this.state.numeroDeCliques2}</button>
     </div>
-    
   }
 }
 
