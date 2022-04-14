ccf42b15ddb2ed5994ee725bee78960d2b4a8bbe
Victor Vidal <victorvidal08@hotmail.com>
2022-04-14 17:05:53 -0300

    outra parcial dos ex do dia

--- Front End/bloco-12-ciclo-de-vida-e-router/dia-1-piadas/src/Dadjoke.js
+++ Front End/bloco-12-ciclo-de-vida-e-router/dia-1-piadas/src/Dadjoke.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Joke from './Joke.js';
 
 class DadJoke extends React.Component {
   constructor() {
     super();
 
-    this.saveJoke = this.saveJoke.bind(this);
+    this.renderJokeElement = this.renderJokeElement.bind(this);
 
     this.state = {
       jokeObj: undefined,
@@ -14,12 +15,17 @@ class DadJoke extends React.Component {
   }
 
   async fetchJoke() {
-    const requestHeaders = { headers: { Accept: 'application/json' } }
-    const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
-    const requestObject = await requestReturn.json();
-    this.setState({
-      jokeObj: requestObject,
-    })
+    this.setState(
+      { loading: true }, // Primeiro parâmetro da setState()!
+      async () => {
+      const requestHeaders = { headers: { Accept: 'application/json' } }
+      const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+      const requestObject = await requestReturn.json();
+      this.setState({
+        loading: false,
+        jokeObj: requestObject
+      });
+    });
   }
 
   componentDidMount() {
@@ -27,11 +33,15 @@ class DadJoke extends React.Component {
   }
 
   saveJoke() {
-    //Esse método será responsável por salvar a piada no array de piadas storedJokes!!
+    this.setState(({ storedJokes, jokeObj }) => ({
+      storedJokes: [...storedJokes, jokeObj]
+    }));
+
+    this.fetchJoke();
   }
 
   render() {
-    const { storedJokes } = this.state;
+    const { storedJokes, loading, jokeObj } = this.state;
     const loadingElement = <span>Loading...</span>;
 
     return (
@@ -40,16 +50,14 @@ class DadJoke extends React.Component {
           {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
         </span>
 
-      {
-        /*
-        Aqui vamos construir nossa lógica com uma renderização condicional
-        do nosso componente Joke, a ideia é renderizar um loading enquanto
-        esperamos a nossa requisição de piadas finalizar.
+        <p>
+          {
+            loading 
+              ? loadingElement
+              : <Joke jokeObj={jokeObj} saveJoke={this.saveJoke} />
+          }
+        </p>
 
-        <p>RENDERIZAÇÃO CONDICIONAL</p>
-        */
-      }
-        <p>teste</p>
       </div>
     );
   }
