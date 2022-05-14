3ca56b8da7ffd142ad15cc4a436566acbd92feed
Victor Vidal <victorvidal08@hotmail.com>
2022-05-14 12:52:37 -0300

    adiciona exemplo mutacao

---
+++ Front End/bloco-15-redux/dia-3-arrays-redux/exemplo1.js
@@ -0,0 +1,5 @@
+const itemPrimario = [1, 2, 3, 4, 5];
+let clonagemTeste = itemPrimario;
+clonagemTeste.concat(6);
+console.log(clonagemTeste); // [1, 2, 3, 4, 5]
+console.log(itemPrimario); // [1, 2, 3, 4, 5]
